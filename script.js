db0f6a67671fb14bf9dbe1229b65c689161e634f
dalernadzhatov <dalernadzhatov@gmail.com>
2025-02-14 19:09:57 +0300

    джава

---
+++ script.js
@@ -0,0 +1,35 @@
+
+let maried = []
+let single = []
+
+let arr2 = [
+    {
+        name: "Ilya",
+        age: 15,
+        single: false
+    },
+    {
+        name: "Ilya 1",
+        age: 18,
+        single: false
+
+    },
+    {
+        name: "Ilya 2",
+        age: 20,
+        single: false
+    },
+    {
+        name: "Ilya 3",
+        age: 25,
+        Maried: true
+    },
+    {
+        name: "Ilya 4",
+        age: 35,
+        Maried: true
+    },
+]
+
+
+
